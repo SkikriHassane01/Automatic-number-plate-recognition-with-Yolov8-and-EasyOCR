dc4cd00aa3545cc2fbf6559aba8bcfee6ddad4fb
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-20 14:52:45 +0100

    load the video and read the frames

--- main.py
+++ main.py
@@ -24,3 +24,16 @@ import cv2
 yolo_model = YOLO("./models/yolov8n.pt")
 license_plate_detector = YOLO('./models/license_plate_detector.pt')
 
+# _______________3. load the video or webcam_______________
+path_video = r"./Video/video1.mp4"
+cap = cv2.VideoCapture(path_video)
+
+num_frame = -1
+vehicles = [2, 3, 5, 7] #id of the vehicles that we want to detect
+
+# _______________4. read the frames_______________
+while True and num_frame < 10:
+    success, img = cap.read()
+
+    if not success:
+        print("Failed to read the frame or end of the video reached.")
