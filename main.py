32672d63b20d94ed47f1eb8d50bf1e47f244de88
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-20 14:52:09 +0100

    import and load the models

--- main.py
+++ main.py
@@ -13,3 +13,14 @@
 """
 # =================================================<<
 # =================================================<<
+
+
+# _______________1. Import libraries_______________
+from logger import logger
+from ultralytics import YOLO
+import cv2
+
+# _______________2. load the models_______________
+yolo_model = YOLO("./models/yolov8n.pt")
+license_plate_detector = YOLO('./models/license_plate_detector.pt')
+
