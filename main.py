68ef0ea85bab8837f645a8204a036a21c9ad2bac
HASSANE SKIKRI <hassaneskikri@gmail.com>
2024-09-20 14:51:43 +0100

    table of content

---
+++ main.py
@@ -0,0 +1,15 @@
+# ================================================>>
+# ================================================>>
+# _______________Table of Content:_________________
+"""
+1. Import libraries
+2. Load the models
+3. load the video or webcam
+4. read the frames 
+5. detect the vehicles
+6. track it using Sort 
+7. detect license plates
+8. ...
+"""
+# =================================================<<
+# =================================================<<
